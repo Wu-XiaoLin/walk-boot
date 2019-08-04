93a11f6b28fccc2bcae8fa739b438d363dd29ed5
wuxiaolin <1132507247@qq.com>
2019-08-04 17:25:17 +0800

    Initial commit

---
+++ src/main/java/cn/wxingzou/walk/WalkApplication.java
@@ -0,0 +1,13 @@
+package cn.wxingzou.walk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/wxingzou/walk/WalkApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.wxingzou.walk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
